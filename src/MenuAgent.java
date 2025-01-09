434f4abb37eecfde059e1329781d80a08d65be3d
Celiane <celianeallaire@gmail.com>
2025-01-09 23:09:06 +0100

    Added MenuAgent gui + updated build

--- src/MenuAgent.java
+++ src/MenuAgent.java
@@ -9,146 +9,26 @@ import java.net.*;
 import java.io.*;
 
 public class MenuAgent extends Agent {
+	private MenuAgentGui myGui;
+	private int[] score; // scores for {player1, tie, player2}
+
 	protected void setup () {
-		//services registration at DF
-		DFAgentDescription dfad = new DFAgentDescription();
-		dfad.setName(getAID());
-		//service no 1
-		ServiceDescription sd1 = new ServiceDescription();
-		sd1.setType("answers");
-		sd1.setName("wordnet");
-		//service no 2
-		ServiceDescription sd2 = new ServiceDescription();
-		sd2.setType("answers");
-		sd2.setName("dictionary");
-		//add them all
-		dfad.addServices(sd1);
-		dfad.addServices(sd2);
-		try {
-			DFService.register(this,dfad);
-		} catch (FIPAException ex) {
-			ex.printStackTrace();
-		}
-		
-		addBehaviour(new WordnetCyclicBehaviour(this));
-		addBehaviour(new DictionaryCyclicBehaviour(this));
-		//doDelete();
+		score = new int[]{0, 0, 0};
+		myGui = new MenuAgentGui(this);
+		myGui.display();
 	}
 	protected void takeDown() {
-		//services deregistration before termination
-		try {
-			DFService.deregister(this);
-		} catch (FIPAException ex) {
-			ex.printStackTrace();
-		}
+		myGui.dispose();
+		System.out.println("Menu agent " + getAID().getLocalName() + " terminated.");
 	}
-	public String makeRequest(String serviceName, String word)
-	{
-		StringBuffer response = new StringBuffer();
-		try
-		{
-			URL url;
-			URLConnection urlConn;
-			DataOutputStream printout;
-			DataInputStream input;
-			url = new URL("http://dict.org/bin/Dict");
-			urlConn = url.openConnection();
-			urlConn.setDoInput(true);
-			urlConn.setDoOutput(true);
-			urlConn.setUseCaches(false);
-			urlConn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-			String content = "Form=Dict1&Strategy=*&Database=" + URLEncoder.encode(serviceName) + "&Query=" + URLEncoder.encode(word) + "&submit=Submit+query";
-			//forth
-			printout = new DataOutputStream(urlConn.getOutputStream());
-			printout.writeBytes(content);
-			printout.flush();
-			printout.close();
-			//back
-			input = new DataInputStream(urlConn.getInputStream());
-			String str;
-			while (null != ((str = input.readLine())))
-			{
-				response.append(str);
-			}
-			input.close();
-		}
-		catch (Exception ex)
-		{
-			System.out.println(ex.getMessage());
-		}
-		//cut what is unnecessary
-		return response.substring(response.indexOf("<hr>")+4, response.lastIndexOf("<hr>"));
+
+	public double[] getScore() {
+		return score;
 	}
+
+	public void playRound() {
+		System.out.println("==== Now Starting a Round ====");
+	}
+
 }
 
-class WordnetCyclicBehaviour extends CyclicBehaviour
-{
-	MenuAgent agent;
-	public WordnetCyclicBehaviour(MenuAgent agent)
-	{
-		this.agent = agent;
-	}
-	public void action()
-	{
-		MessageTemplate template = MessageTemplate.MatchOntology("wordnet");
-		ACLMessage message = agent.receive(template);
-		if (message == null)
-		{
-			block();
-		}
-		else
-		{
-			//process the incoming message
-			String content = message.getContent();
-			ACLMessage reply = message.createReply();
-			reply.setPerformative(ACLMessage.INFORM);
-			String response = "";
-			try
-			{
-				response = agent.makeRequest("wn",content);
-			}
-			catch (NumberFormatException ex)
-			{
-				response = ex.getMessage();
-			}
-			reply.setContent(response);
-			agent.send(reply);
-		}
-	}
-}
-
-class DictionaryCyclicBehaviour extends CyclicBehaviour
-{
-	MenuAgent agent;
-	public DictionaryCyclicBehaviour(MenuAgent agent)
-	{
-		this.agent = agent;
-	}
-	public void action()
-	{
-		MessageTemplate template = MessageTemplate.MatchOntology("dictionary");
-		ACLMessage message = agent.receive(template);
-		if (message == null)
-		{
-			block();
-		}
-		else
-		{
-			//process the incoming message
-			String content = message.getContent();
-			ACLMessage reply = message.createReply();
-			reply.setPerformative(ACLMessage.INFORM);
-			String response = "";
-			try
-			{
-				response = agent.makeRequest("english", content);
-			}
-			catch (NumberFormatException ex)
-			{
-				response = ex.getMessage();
-			}
-			reply.setContent(response);
-			agent.send(reply);
-		}
-	}
-}
