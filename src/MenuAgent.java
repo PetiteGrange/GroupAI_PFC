eae4e1be9c82c5e4bfba5be6eed433a0cb63a62d
PetiteGrange <134615787+PetiteGrange@users.noreply.github.com>
2025-01-11 13:04:09 +0100

    Add step 0 to the communication structure

--- src/MenuAgent.java
+++ src/MenuAgent.java
@@ -6,11 +6,13 @@ import jade.lang.acl.*;
 import jade.domain.*;
 import jade.domain.FIPAAgentManagement.*;
 import java.net.*;
+import java.nio.file.attribute.AclEntryPermission;
 import java.io.*;
 
 public class MenuAgent extends Agent {
 	private MenuAgentGui myGui;
 	private int[] score; // scores for {player1, tie, player2}
+	private AID[] playerAgents; // list of known player agents
 
 	protected void setup () {
 		score = new int[]{0, 0, 0};
@@ -41,7 +43,20 @@ public class MenuAgent extends Agent {
 		public void action() {
 			switch (step) {
 				case 0: // Cette partie consiste à envoyer un message à l'agent joueur pour lui demander de jouer
+					ACLMessage request = new ACLMessage(ACLMessage.REQUEST);
 					
+					//On ajoute les agents joueurs à la liste des destinataires
+					for (AID playerAgent : playerAgents) {
+						request.addReceiver(playerAgent);
+					}
+
+					//TODO: Need to think about the content of the message
+
+					myAgent.send(request);
+					mt = MessageTemplate.MatchInReplyTo(request.getReplyWith());
+					
+					step++; // On passe à l'étape suivante
+
 					break;
 
 				case 1: // Cette partie consiste à recevoir le message de l'agent joueur
