b7dfd958d971115095e409f3605df2a1560a7cdc
Celiane <celianeallaire@gmail.com>
2025-01-13 15:27:23 +0100

    move ControllerBehaviour into MenuAgent class + update steps for action() + create done condition

--- src/MenuAgent.java
+++ src/MenuAgent.java
@@ -34,67 +34,86 @@ public class MenuAgent extends Agent {
 		// TODO UPDATE THE LIST OF KNOWN PLAYERS
 	}
 
-}
-
 	private class ControllerBehaviour extends Behaviour {
+		private String[] playerActions = new String[2]; // actions for {player1, player2}
 		private int step = 0;
 		private int repliesCnt = 0;
+		private MessageTemplate mt;
+		private long start = System.currentTimeMillis(); //Used to track time, check if player is taking too long
 
 
 		public void action() {
 			switch (step) {
-				case 0: // Cette partie consiste à envoyer un message à l'agent joueur pour lui demander de jouer
-					ACLMessage request = new ACLMessage(ACLMessage.REQUEST); // pas sûr du type de message, CFP peut etre aussi très pratique
-					
-					//On ajoute les agents joueurs à la liste des destinataires
+				case 0: //Step 0: Send a CFP to all players
+					//call for action to found players
+					ACLMessage cfp = new ACLMessage(ACLMessage.CFP); // Solicit player for it's action
+					//Add players to the list of receivers
 					for (AID playerAgent : playerAgents) {
-						request.addReceiver(playerAgent);
+						cfp.addReceiver(playerAgent);
 					}
+					cfp.setConversationId("RPS-game");
+					myAgent.send(cfp);
 
-					//TODO: Need to think about the content of the message
-
-					myAgent.send(request);
-					mt = MessageTemplate.MatchInReplyTo(request.getReplyWith());
-
-					step++; // On passe à l'étape suivante
-
+					mt = MessageTemplate.and(MessageTemplate.MatchConversationId("RPS-game"),
+							MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
+					System.out.println("CFP sent to all found players :" + playerAgents.length);
+					step = 1;
 					break;
 
-				case 1: // Cette partie consiste à recevoir le message de l'agent joueur
-
+				case 1: //Step 1: Receive and Wait for the players' actions
 					ACLMessage reply = myAgent.receive(mt);
+					long end = System.currentTimeMillis();
+					if (end - start >= 10000) { //Adapt condition aka wait time
+						System.out.println("Time has expired for step 1. The game is canceled due to lack of response from a player");
+						step = 3;
+						break;
+					}
 					if (reply != null) {
-						if (reply.getPerformative() == ACLMessage.INFORM) {
-
-							//TODO: Parser la réponse des joueurs
-
+						if (reply.getPerformative() == ACLMessage.PROPOSE) {
+							playerActions[repliesCnt] = reply.getContent();
 							repliesCnt++;
-							if (repliesCnt == 2) {
+							if (repliesCnt == 1) {
 								//all proposals have been received
-								step = 2; 
+								step = 2;
 							}
-
 						}
 					} else {
 						block();
 					}
-					
 					break;
 
-				case 2: // Cette partie consiste à déterminer le gagnant
-					
-					break;
-			
-				default: // PLACEHOLDER
+				case 2: // Step 2: Determine the winner thanks to the players' actions
+					//Checking the players' submitted actions
+					for (int i = 0; i < 2; i++){
+						if ( !playerActions[i].equals("rock") && !playerActions[i].equals("paper") && !playerActions[i].equals("scissors")) {
+							System.out.println("Player" + i + " did not respond properly. The game is canceled.");
+							step = 3;
+							break;
+						}
+					}
+					if (playerActions[0].equals(playerActions[1])) {
+						System.out.println("It's a tie! Both players played " + playerActions[0]);
+						score[1]++;
+					} else if (playerActions[0].equals("rock") && playerActions[1].equals("scissors") ||
+							playerActions[0].equals("paper") && playerActions[1].equals("rock") ||
+							playerActions[0].equals("scissors") && playerActions[1].equals("paper")) {
+						System.out.println("Player 1 wins!");
+						score[0]++;
+					} else {
+						System.out.println("Player 2 wins!");
+						score[2]++;
+					}
+					step = 3;
 					break;
 			}
 		}
 
-		
 		public boolean done() { // PLACEHOLDER to finish the behaviour
-			return false;
+			//process terminates here if the game is canceled or if the game is finished
+			return step == 3;
 		}
 	}
+}
 
 
 
